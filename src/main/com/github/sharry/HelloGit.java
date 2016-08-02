f097e95191bc403128244d8395de577bb32ed292
Sharry Shuai <sharryshuai@ML00070098.local>
2016-08-02 12:34:06 +0800

    init project

---
+++ src/main/com/github/sharry/HelloGit.java
@@ -0,0 +1,9 @@
+package main.com.github.sharry;
+
+public class HelloGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git!\r\n\t---My first git project");
+    }
+
+}
