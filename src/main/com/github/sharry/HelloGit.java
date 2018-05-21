9b2990ca6335259e3a3e35b5c73f509677d85b44
Sharry Shuai <Sharry.Shuai@activenetwork.com>
2018-05-21 13:20:28 +0800

    update HelloGit.java

--- src/main/com/github/sharry/HelloGit.java
+++ src/main/com/github/sharry/HelloGit.java
@@ -2,6 +2,10 @@ package main.com.github.sharry;
 
 public class HelloGit {
 
+    public void greet(String name) {
+        System.out.println("Hello, this is " + name);
+    }
+    
     public static void main(String[] args) {
         System.out.println("Hello Git!\r\n\t---My first git project");
     }
